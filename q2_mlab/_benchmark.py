54fbd71c74eec674a9bb214a2408a3023a6345cc
Imran McGrath <i.mcgrath207@gmail.com>
2020-07-09 14:56:29 -0700

    Added unregistered _unit_benchmark that returns best model

--- q2_mlab/_benchmark.py
+++ q2_mlab/_benchmark.py
@@ -30,3 +30,32 @@ def unit_benchmark(
     results_table = worker.tabularize()
 
     return results_table
+
+
+def _unit_benchmark(
+    table: biom.Table,
+    metadata: pd.Series,
+    algorithm: str,
+    params: str,
+    distance_matrix: DistanceMatrix = None,
+    n_repeats: int = 3,
+) -> pd.DataFrame:
+
+    if algorithm in RegressionTask.algorithms:
+        worker = RegressionTask(
+            table, metadata, algorithm, params, n_repeats, distance_matrix
+        )
+    elif algorithm in ClassificationTask.algorithms:
+        worker = ClassificationTask(
+            table, metadata, algorithm, params, n_repeats, distance_matrix
+        )
+    else:
+        raise ValueError(algorithm + " is not an accepted algorithm")
+
+    for train_index, test_index in worker.splits:
+        worker.cv_fold(train_index, test_index)
+        print(worker.best_accuracy)
+
+    results_table = worker.tabularize()
+    print("final: ", worker.best_accuracy)
+    return results_table, worker.best_model
