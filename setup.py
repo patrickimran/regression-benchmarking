cbda54a8b67f79b50039a54dec195ce2cdc68f52
Imran McGrath <i.mcgrath207@gmail.com>
2020-08-20 08:56:43 -0700

    FIX formatting and add tqdm as requirement

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
         'qiime2.plugins': ['q2-mlab=q2_mlab.plugin_setup:plugin'],
         'console_scripts': ['orchestrator=q2_mlab.orchestrator:cli'],
     },
-    package_data={'q2_mlab': ['assets/index.html', 
+    package_data={'q2_mlab': ['assets/index.html',
                               'citations.bib',
                               'templates/array_job_template.sh']},
     zip_safe=False,
-    install_requires=['xgboost', 'calour', 'click']
+    install_requires=['xgboost', 'calour', 'click', 'tqdm']
 )
